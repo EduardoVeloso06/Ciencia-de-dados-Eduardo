096b64219b2b875fb344e3851ae898bb900daf45
Eduardo Veloso <107579577+EduardoVeloso06@users.noreply.github.com>
2024-11-21 16:43:56 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,9 @@
 const url = 'https://raw.githubusercontent.com/EduardoVeloso06/Ciencia-de-dados-Eduardo/refs/heads/main/dados-globais.json';
+
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url);
+
+    const dados = await res.json();
+}
+
+visualizarInformacoesGlobais ();
